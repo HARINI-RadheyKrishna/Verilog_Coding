df3ed9e9b88faa793f12239770af3067d9dd25df
HARINI <89853508+HARINI-RadheyKrishna@users.noreply.github.com>
2023-03-10 18:12:24 -0800

    Updation due to errors
    Modified the code to correct the errors.

--- ShiftReg_tb.c
+++ ShiftReg_tb.c
@@ -18,18 +18,18 @@ module tb;
         end
     end
 
-    shiftreg(.d(d2), .clk(clk2), .rstneg(rstneg2), .q(q2));
+    // shiftreg(.d(d2), .clk(clk2), .rstneg(rstneg2), .q(q2));
 
-    initial begin
-        d2 <= 0;
-        clk2 <= 0;
-        rstneg2 <= 0
+    // initial begin
+    //     d2 <= 0;
+    //     clk2 <= 0;
+    //     rstneg2 <= 0;
 
-        $monitor ("d = %0b clk = %0b rstneg = %0b q = %0b", d1, clk1, rstneg1, q1);
+    //     $monitor ("d = %0b clk = %0b rstneg = %0b q = %0b", d1, clk1, rstneg1, q1);
 
-        for (ii = 0; ii < 8; ii = ii + 1)
-            {rstneg2, d2, clk2} = ii; 
-            #10;
-        end
-    end
+    //     for (ii = 0; ii < 8; ii = ii + 1)
+    //         {rstneg2, d2, clk2} = ii; 
+    //         #10;
+    //     end
+    // end
 endmodule
